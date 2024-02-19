df9e898ad332b110c8ecad3135bf679b41be5e32
STANLEY <Leystan61@gmail.com>
2024-02-19 23:33:02 +0300

    Successfully proved that JAR dependencies were accepted by using FLATLAF

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import com.formdev.flatlaf.themes.FlatMacLightLaf;
+
+import javax.swing.*;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -7,8 +10,19 @@ public class Main {
         //Hello World to prove that the program works.
         System.out.println("HELLO WORLD!!");
 
-        // STABLE
-        // Dev
+        //Start the Swing Application with  a try catch block.
+        try
+        {
+            //try setting the Default look and Feel to use FlatLaf MACOS Light.
+            UIManager.setLookAndFeel(new FlatMacLightLaf());
+
+            //Load the SplashScreen.
+        }
+        catch(Exception EX)
+        {
+            //print out the exception.
+            System.out.println("Failed to Load SplashScreen with Exception" + EX);
+        }
 
 
     }
