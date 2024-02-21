bc0a0782f0bc3bf34039c164f4d666ba3c20ac7e
STANLEY <Leystan61@gmail.com>
2024-02-21 13:33:39 +0300

    Added all Dependencies needed for my project

--- src/aa_UI_Login_and_Startup_Windows/Splash_Screen_Window.java
+++ src/aa_UI_Login_and_Startup_Windows/Splash_Screen_Window.java
@@ -1,4 +1,24 @@
+/*
+    PROGRAM     :  LIGHTNING TRADER WORKSTATION.
+*   AUTHOR      :   STANLEY ANDREW KINYUA
+*   LICENSE     :   None
+*   COPYRIGHT   :   2024.
+
+*   CLASS   -   SPLASH_SCREEN_WINDOW
+
+    USE     -   Draws a splash Screen that will be used during App Startup.
+*/
+
 package aa_UI_Login_and_Startup_Windows;
 
-public class Splash_Screen_Window {
+import com.formdev.flatlaf.themes.FlatMacLightLaf;
+import javax.swing.*;
+
+public class Splash_Screen_Window extends JFrame
+{
+    //This is a blank Window with Some Transparency and some Text.
+
+
+
+
 }
