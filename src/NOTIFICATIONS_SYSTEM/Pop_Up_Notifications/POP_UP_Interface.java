a5cd0df848302bab89bfe82285b43efc14b2ed12
ANDREW <Leystan61@gmail.com>
2024-03-24 17:26:48 +0300

    Created Pop_Ups to Display Positive and Negative Information

--- src/NOTIFICATIONS_SYSTEM/Pop_Up_Notifications/POP_UP_Interface.java
+++ src/NOTIFICATIONS_SYSTEM/Pop_Up_Notifications/POP_UP_Interface.java
@@ -1,8 +1,12 @@
 package NOTIFICATIONS_SYSTEM.Pop_Up_Notifications;
 
-/** PO*/
+/** Pop UP interface is used to declare functions that pop up messages will Call*/
 public interface POP_UP_Interface
 {
 
-    void Show_Pop_up();
+    /** Display the Popup notification window*/
+    void Show_Pop_up(int x , int y);
+
+    /** Registers the PopUp Window with the PopUp manager*/
+    void Register_PopUp_Window();
 }
