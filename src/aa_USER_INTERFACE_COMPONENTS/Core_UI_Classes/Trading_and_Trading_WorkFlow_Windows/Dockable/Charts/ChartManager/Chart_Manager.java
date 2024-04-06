420d59fac854c198df345ee2265628a709017659
ANDREW <Leystan61@gmail.com>
2024-04-06 20:44:58 +0300

    Created Chart Manager and made some Improvements on Chart MANAGER

--- src/aa_USER_INTERFACE_COMPONENTS/Core_UI_Classes/Trading_and_Trading_WorkFlow_Windows/Dockable/Charts/ChartManager/Chart_Manager.java
+++ src/aa_USER_INTERFACE_COMPONENTS/Core_UI_Classes/Trading_and_Trading_WorkFlow_Windows/Dockable/Charts/ChartManager/Chart_Manager.java
@@ -3,15 +3,63 @@ package aa_USER_INTERFACE_COMPONENTS.Core_UI_Classes.Trading_and_Trading_WorkFlo
 /** This class is used to Open and Manage Charts +  store Chart Settings + Chart Template Information */
 public class Chart_Manager
 {
+    private int Open_Charts = 0;
+
+    //Special Data Types to track which Charts are open.
+   private boolean[] Charts_Array = new boolean[30];
+
+
     /** Default Constructor for Chart Manager*/
     public Chart_Manager()
+    {
+        String Locked_Positions_FromConfigFile = null;
+
+        // On Load : Read the Config Files and Determine Open Charts if any
+        Read_From_Config_File();
+
+        //Lock Chart Positions in Array after reading the Locked Positions
+        Lock_Multiple_ChartPositions_in_ChartsArray(Locked_Positions_FromConfigFile);
+
+        // Lock Chart Positions in Chart Array if any are Open
+
+    }
+
+    // Chart Manager Functions that Open and Manage Number of Open Charts
+    public void Open_Chart(int Chart_Number ,String Symbol , String Chart_Type , int Period)
+    {
+        //Read Charts Array and Determine Open Charts
+    }
+    public void Close_Chart(int Chart_Number)
     {
 
     }
 
+    /** Functions to manipulate Chart Array*/
 
-    //Chart Manager Supports Opening upto 30 Charts which can be accessed by their Individual names
-    public void OpenChart1(){}
+    public void Lock_Multiple_ChartPositions_in_ChartsArray(String Positions_toLock)
+    {
+        //Split String PositionsTo Lock and extract positions to be unlocked and add them to an array
+
+    }
+    public void Lock_Chart_Position_in_ChartsArray(){}
+    public void Unlock_Chart_Position_in_ChartsArray(){}
+
+    // Chart Manager Config File Reading Functions
+    public void Write_to_Config_File()
+    {
+        //Concatenate Open Positions
+    }
+    public void Read_From_Config_File(){}
+
+
+
+
+
+    //Chart Manager Supports Opening up to 30 Charts which can be accessed by their Individual names
+    public void OpenChart1(String Symbol , String Chart_Type , int Period, String DockPosition , String Dock_Parent)
+    {
+        //Create new Chart Object and Spawn Window
+    }
     public void OpenChart2(){}
     public void OpenChart3(){}
     public void OpenChart4(){}
@@ -29,6 +77,9 @@ public class Chart_Manager
     public void OpenChart16(){}
     public void OpenChart17(){}
     public void OpenChart18(){}
+
+
+
 
 
 }
--- src/aa_USER_INTERFACE_COMPONENTS/Core_UI_Classes/Trading_and_Trading_WorkFlow_Windows/Dockable/Charts/XY_CHART.java
+++ src/aa_USER_INTERFACE_COMPONENTS/Core_UI_Classes/Trading_and_Trading_WorkFlow_Windows/Dockable/Charts/XY_CHART.java
@@ -3,5 +3,6 @@ package aa_USER_INTERFACE_COMPONENTS.Core_UI_Classes.Trading_and_Trading_WorkFlo
 /** The default class that all XY axis charts inherit from */
 public abstract class XY_CHART implements  Generic_XY_Chart_Interface
 {
+
 }
 
