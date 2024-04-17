46bee9ae755af8dcc9dfdaa5a8b401eeee68b48a
ANDREW <Leystan61@gmail.com>
2024-04-17 22:45:50 +0300

    Improved Chart Window till the point where I can begin to draw the Axis

--- src/aa_UI/Trading_Windows/Dockable/Charts_and_Objects/Charts/Generic_XY_Chart/Chart_Canvas.java
+++ src/aa_UI/Trading_Windows/Dockable/Charts_and_Objects/Charts/Generic_XY_Chart/Chart_Canvas.java
@@ -1,4 +1,35 @@
 package aa_UI.Trading_Windows.Dockable.Charts_and_Objects.Charts.Generic_XY_Chart;
 
-public class Chart_Canvas {
+import javax.swing.*;
+import java.awt.*;
+
+public class Chart_Canvas extends JPanel
+{
+    public int Width;
+    public int Height;
+
+   
+
+    /**Constructor for Class */
+    public Chart_Canvas()
+    {
+
+        //Set Background Colour to white  TODO LATER change to based on Theme
+        this.setBackground(Color.white);
+
+        //Initialize Java 2D Libraries to allow drawing.
+
+        // Plot X A
+
+
+
+    }
+
+
+
+    /**Plot the X and Y Axis */
+    private void Plot_Axis_XY()
+    {
+
+    }
 }
--- src/aa_UI/Trading_Windows/Dockable/Charts_and_Objects/Charts/Generic_XY_Chart/_Generic_XY_Chart.java
+++ src/aa_UI/Trading_Windows/Dockable/Charts_and_Objects/Charts/Generic_XY_Chart/_Generic_XY_Chart.java
@@ -3,7 +3,6 @@ package aa_UI.Trading_Windows.Dockable.Charts_and_Objects.Charts.Generic_XY_Char
 import net.miginfocom.swing.MigLayout;
 
 import javax.swing.*;
-import java.awt.*;
 
 /**Class that all XY Trading Charts inherit From */
 public class _Generic_XY_Chart extends JPanel
@@ -13,51 +12,35 @@ public class _Generic_XY_Chart extends JPanel
 
     public _Generic_XY_Chart()
     {
-        //Set the Layout of the Chart
+        //Set the Layout of the Chart to absolute Positioning
         this.setLayout(new MigLayout());
 
+
         //Initialize the Chart Area
-        Initialize_ChartArea();
+        Chart_Canvas  Chart_Canvas = new Chart_Canvas();
 
-        //Plot the X Axis
+        //Add Chart Canvas to the Chart , it should occupy 85% of the width and 75% of the Height
+        this.add(Chart_Canvas,"x 10,y 10, w 95% , h 95%");
 
+        var width   = Chart_Canvas.getBounds().getWidth();
+        var height  = Chart_Canvas.getBounds().getHeight();
+        var h = Chart_Canvas.getWidth();
 
-
-
-
-        //Plot the Y Axis
-
-
-    }
-
-    private void Initialize_ChartArea()
-    {
-        //Create a Chart Canvas where the Chart will be plotted.
-        JPanel Chart_Canvas = new JPanel();
-            //Set the Layout
-        Chart_Canvas.setLayout(new MigLayout());
-            //Set Background Colour to white  TODO LATER change to based on Theme
-        Chart_Canvas.setBackground(Color.white);
+        System.out.println(width);
+        System.out.println(height);
 
 
 
-            //Add Chart Canvas to the Chart and it should occuppy 85% of the width and 75% of the Height
-        this.add(Chart_Canvas,"w 85% , h 75%");
 
-        int Width = Chart_Canvas.getWidth();
-        int Height = Chart_Canvas.getHeight();
 
-        String Height_String = Integer.toString(Height);
-        String Width_String = Integer.toString(Width);
 
-        System.out.println("The Height and Width of Chart Canvas is : " + Height_String + Width_String );
+
+
+
 
     }
 
-    private void Plot_X_Axis()
-    {
 
-    }
 
 
 }
