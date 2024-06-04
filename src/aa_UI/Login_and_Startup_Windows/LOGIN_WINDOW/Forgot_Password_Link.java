431a43553c1c0bcedb46284e94e05fe8b32be141
STANLEY <Leystan61@gmail.com>
2024-06-04 11:24:10 +0300

    Created a Forgot Password Link

--- src/aa_UI/Login_and_Startup_Windows/LOGIN_WINDOW/Forgot_Password_Link.java
+++ src/aa_UI/Login_and_Startup_Windows/LOGIN_WINDOW/Forgot_Password_Link.java
@@ -3,6 +3,7 @@ package aa_UI.Login_and_Startup_Windows.LOGIN_WINDOW;
 import org.jdesktop.swingx.JXHyperlink;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Forgot_Password_Link extends JLabel
 {
@@ -12,6 +13,9 @@ public class Forgot_Password_Link extends JLabel
 
         // Set the Text of the Label
         this.setText("FORGOT PASSWORD");
+
+        //TODO   Set the Colour of the Font
+        this.setForeground(Color.ORANGE);
 
         //TODO  Create an Event Listener to Change Colour on Hover + change Mouse Icon to Finger
         //TODO  Create an Event Listener to Listen for clicks
