f5037d74cf7477b593a848e7ada6ce81b120113c
STANLEY <Leystan61@gmail.com>
2024-10-08 21:49:35 +0300

    Created Sample xml config Files though I will use text not xml

--- src/Main.java
+++ src/Main.java
@@ -46,6 +46,8 @@ public class Main {
         //sTART Capital
       // CAPITAL_dotCOM_RestWebSocket_Engine CDC = new CAPITAL_dotCOM_RestWebSocket_Engine();
 
+        //TODO Dukascopy Engine to Start here for testing
+
 
 
         //TODO Read Config File and determine whether to perform first load actions. load Dark/ Light Mode
